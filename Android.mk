023405e52b0612cd70dbd99e195ece3f22f15d54
sluzky <simon@soom.la>
2014-08-31 12:29:37 +0300

    new structure iOS working

---
+++ Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos2dx_profile_static
+LOCAL_MODULE_FILENAME := libcocos2dxprofile
+LOCAL_SRC_FILES := Profile/CCProfileConsts.cpp \
+	Profile/CCProfileController.cpp \
+	Profile/CCProfileEventDispatcher.cpp \
+	Profile/CCProfileService.cpp \
+	Profile/CCSocialActionUtils.cpp \
+	Profile/CCUserProfileUtils.cpp \
+	Profile/domain/CCUserProfile.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Profile
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Profile/domain
+
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_soomla_common_static
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../..
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../Profile
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../Profile/domain
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,extensions/soomla-cocos2dx-core/android/jni)
