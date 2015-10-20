437f7a697844b4307dd15126e40b050e29a4c52c
Eugene Butusov <EvgenyButusov@gmail.com>
2015-10-20 12:55:08 +0300

    [IMPROVE] Arrange param names in cocos2d-x
    create class basement

---
+++ Soomla/CCSoomlaProfileConfigBuilder.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Eugene Butusov on 20/10/15.
+// Copyright (c) 2015 SOOMLA. All rights reserved.
+//
+
+#include "CCSoomlaProfileConfigBuilder.h"
---
+++ Soomla/CCSoomlaProfileConfigBuilder.h
@@ -0,0 +1,18 @@
+//
+// Created by Eugene Butusov on 20/10/15.
+// Copyright (c) 2015 SOOMLA. All rights reserved.
+//
+
+#ifndef COCOS2DXPROFILE_CCSOOMLAPROFILECONFIGBUILDER_H
+#define COCOS2DXPROFILE_CCSOOMLAPROFILECONFIGBUILDER_H
+
+namespace soomla {
+    class CCSoomlaProfileConfigBuilder : public CCSoomlaConfigBuilder {
+
+    };
+}
+
+
+
+
+#endif //COCOS2DXPROFILE_CCSOOMLAPROFILECONFIGBUILDER_H
