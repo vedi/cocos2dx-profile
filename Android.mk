e26eb47af991dc9ed4a973aa56a662c554d7594a
Eugene Butusov <EvgenyButusov@gmail.com>
2015-10-22 18:22:02 +0300

    update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,10 +6,12 @@ LOCAL_MODULE := cocos2dx_profile_static
 LOCAL_MODULE_FILENAME := libcocos2dxprofile
 
 PROFILE_SRC_LIST := $(wildcard $(LOCAL_PATH)/Soomla/*.cpp)
+PROFILE_SRC_LIST += $(wildcard $(LOCAL_PATH)/Soomla/config/*.cpp)
 PROFILE_SRC_LIST += $(wildcard $(LOCAL_PATH)/Soomla/domain/*.cpp)
 LOCAL_SRC_FILES := $(PROFILE_SRC_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/Soomla
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Soomla/config
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/Soomla/domain
 
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
@@ -17,6 +19,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_soomla_common_static
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/Soomla
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/Soomla/config
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/Soomla/domain
 
 include $(BUILD_STATIC_LIBRARY)
