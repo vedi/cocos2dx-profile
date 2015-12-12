c81fbedd385e8632f26b5969ab7d9eb75444b095
Eugene Butusov <EvgenyButusov@gmail.com>
2015-12-12 10:10:52 +0300

    Remove useless invocations of CCDomain methods add Name and IconURL to Leaderboard

--- Soomla/domain/CCLeaderboard.cpp
+++ Soomla/domain/CCLeaderboard.cpp
@@ -29,17 +29,29 @@ bool soomla::CCLeaderboard::init(cocos2d::__String *id, cocos2d::__String *provi
 
 
 bool soomla::CCLeaderboard::initWithDictionary(cocos2d::__Dictionary *dict) {
-    CCDomain::initWithDictionary(dict);
+
+    fillIdFromDict(dict);
     fillProviderFromDict(dict);
-    fillNameFromDict(dict);
-    fillIconUrlFromDict(dict);
+    if (dict->valueForKey(CCProfileConsts::JSON_NAME) != nullptr) {
+        fillNameFromDict(dict);
+    } else {
+        mName = cocos2d::__String::create("");
+    }
+    if (dict->valueForKey(CCProfileConsts::JSON_ICON_URL) != nullptr) {
+        fillIconUrlFromDict(dict);
+    } else {
+        mIconUrl = cocos2d::__String::create("");
+    }
+
 
     return true;
 }
 
 cocos2d::__Dictionary *soomla::CCLeaderboard::toDictionary() {
-    cocos2d::__Dictionary* dict = CCDomain::toDictionary();
+    cocos2d::__Dictionary* dict = cocos2d::__Dictionary::create();
 
+    dict->setObject(cocos2d::__String::create(CCProfileConsts::JSON_JSON_TYPE_LEADERBOARD), CCCoreConsts::JSON_JSON_TYPE);
+    putIdToDict(dict);
     putProviderToDict(dict);
     putNameToDict(dict);
     putIconUrlToDict(dict);
@@ -48,6 +60,7 @@ cocos2d::__Dictionary *soomla::CCLeaderboard::toDictionary() {
 }
 
 soomla::CCLeaderboard::~CCLeaderboard() {
+    CC_SAFE_RELEASE(mId);
     CC_SAFE_RELEASE(mProvider);
     CC_SAFE_RELEASE(mName);
     CC_SAFE_RELEASE(mIconUrl);
